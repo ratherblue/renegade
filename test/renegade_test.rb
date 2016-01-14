95662f3b794dfe756d60d4d6ed90ba7aa8b2889d
ratherblue <ratherblue@gmail.com>
2016-01-13 17:17:35 -0800

    fixing tests

--- test/renegade_test.rb
+++ test/renegade_test.rb
@@ -5,8 +5,4 @@ class RenegadeTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Renegade::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end

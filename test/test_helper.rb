e3e9c4ecdb99cfa25e122c8e779ac12234b7cb37
ratherblue <ratherblue@gmail.com>
2016-01-14 15:28:56 -0800

    moving coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,8 @@
+require 'coveralls'
+
+Coveralls.wear!
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'renegade'
 require 'minitest/autorun'
-require 'coveralls'
-
-Coveralls.wear!

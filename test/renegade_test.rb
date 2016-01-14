31a03f7eceadcca151722ed299b690b68923bf30
ratherblue <ratherblue@gmail.com>
2016-01-14 11:35:13 -0800

    adding parenthesis

--- test/renegade_test.rb
+++ test/renegade_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 # Renegade tests
 class RenegadeTest < Minitest::Test
-  def test_that_it_has_a_version_number
+  def test_that_it_has_a_version_number()
     refute_nil ::Renegade::VERSION
   end
 end

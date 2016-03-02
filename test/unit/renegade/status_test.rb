005d05d3bab734a2a778ccae071f166c7fe6050c
ratherblue <ratherblue@gmail.com>
2016-03-02 15:50:51 -0800

    fix test phrasing

--- test/unit/renegade/status_test.rb
+++ test/unit/renegade/status_test.rb
@@ -12,22 +12,22 @@ describe Renegade::Status do
     $stdout = STDOUT
   end
 
-  it 'shows passed status' do
+  it 'should show passed status' do
     subject.report('Good Label', true)
     $stdout.string.must_equal("\e[32m  √ Good Label\e[0m\n")
   end
 
-  it 'shows failed status' do
+  it 'should show failed status' do
     subject.report('Bad Label', false)
     $stdout.string.must_equal("\e[31m  × Bad Label\e[0m\n")
   end
 
-  it 'shows warning status' do
+  it 'should show warning status' do
     subject.report('Warning Label', false, true)
     $stdout.string.must_equal("\e[33m  × Warning Label\e[0m\n")
   end
 
-  it 'shows hook starting' do
+  it 'should show hook starting' do
     subject.hook_start('Hook Name')
     $stdout.string.must_equal("\n\e[35mRunning Hook Name hooks…\e[0m\n")
   end
